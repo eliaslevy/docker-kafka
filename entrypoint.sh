6e8f043fd457fb8c1c88ad4ba87e4048f2fd1d9b
Elias Levy <fearsome.lucidity@gmail.com>
2017-10-13 15:49:51 -0700

    Fix config generation.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Only configure the system the first time this container starts. A Kubernetes volume mount 
-# will preserve the config if the container dies and is restarted in the same node.
-if [ ! -f /kafka/config/server.properties ]; then
-  # Create a ZK connection string for the servers and the root.
-  ZOOKEEPER_CONNECT=$ZOOKEEPER_SERVERS${ZOOKEEPER_ROOT:=/kafka}
+# Create a ZK connection string for the servers and the root.
+ZOOKEEPER_CONNECT=$ZOOKEEPER_SERVERS${ZOOKEEPER_ROOT:=/kafka}
 
-  if [ -z $ADVERTISED_HOST_NAME ]; then
-    ADVERTISED_HOST_NAME=$(hostname -f)
-    echo "Advertizing host name ${ADVERTISED_HOST_NAME}."
-  fi
-
-  # Create the config file.
-  sed -i \
-      -e "s|\${ADVERTISED_HOST_NAME}|$ADVERTISED_HOST_NAME|g " \
-      -e "s|\${ZOOKEEPER_CONNECT}|$ZOOKEEPER_CONNECT|g" /kafka/templates/server.properties
+if [ -z $ADVERTISED_HOST_NAME ]; then
+  ADVERTISED_HOST_NAME=$(hostname -f)
+  echo "Advertizing host name ${ADVERTISED_HOST_NAME}."
 fi
+
+# Create the config file.
+sed -i \
+    -e "s|\${ADVERTISED_HOST_NAME}|$ADVERTISED_HOST_NAME|g" \
+    -e "s|\${ZOOKEEPER_CONNECT}|$ZOOKEEPER_CONNECT|g" /kafka/config/server.properties
 
 cd /kafka
 exec "$@"
